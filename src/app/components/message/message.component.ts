16f330abd3b84f1d630dab47179bb1a70f652904
mostfa <mostfalachheb20@gmail.com>
2023-07-24 17:14:58 +0200

    message

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-message',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
-
-  constructor() { }
+  userconnect = JSON.parse(localStorage.getItem("userconnect")!);
+  messages!:any[];
+  constructor(private message:MessageService) { }
 
   ngOnInit(): void {
+    this.messagebycondidat()
+  
   }
-
+  messagebycondidat() {
+    this.message.getmessage().subscribe((res: any) => {
+      console.log(res,"kkk")
+      // Filter messages where condidatId and sender match the userconnect's condidatId
+      this.messages = res.data.filter((message: any) => {
+        return (
+          message.condidatId === this.userconnect.user.condidatId._id
+          // message.sender === this.userconnect.user.condidatId
+        );
+      });
+      console.log("messages", this.messages);
+    });
+  }
 }
